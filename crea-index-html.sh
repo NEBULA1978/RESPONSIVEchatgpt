ce1c53e16e0e5ee9abbb939201e816d3a61bb261
NEBULA1978 <rpascualey@gmail.com>
2023-09-16 01:01:48 +0200

    Creo index.html con link a css con crea-index-html.sh modificado

--- crea-index-html.sh
+++ crea-index-html.sh
@@ -8,10 +8,25 @@ create_css_file() {
     echo "@media (max-width: $size) {" >> "css/$filename"
     echo "    /* Your styles for max-width $size go here */" >> "css/$filename"
     echo "}" >> "css/$filename"
-    echo "<link rel=\"stylesheet\" href=\"css/$filename\" media=\"(max-width: $size)\">" >> "index.html"
 }
 
 # Create CSS files for different screen sizes
 create_css_file "768px"
 create_css_file "1024px"
 create_css_file "1920px"
+
+# Create the index.html file with the provided content and dynamically add CSS links
+echo '<!DOCTYPE html>' > "index.html"
+echo '<html lang="en">' >> "index.html"
+echo '<head>' >> "index.html"
+echo '    <meta charset="UTF-8">' >> "index.html"
+echo '    <meta name="viewport" content="width=device-width, initial-scale=1.0">' >> "index.html"
+echo '    <title>Document</title>' >> "index.html"
+echo '    <!-- Enlaces a los archivos CSS generados por el script -->' >> "index.html"
+echo '    <link rel="stylesheet" href="css/style-768px.css" media="(max-width: 768px)">' >> "index.html"
+echo '    <link rel="stylesheet" href="css/style-1024px.css" media="(max-width: 1024px)">' >> "index.html"
+echo '    <link rel="stylesheet" href="css/style-1920px.css" media="(max-width: 1920px)">' >> "index.html"
+echo '</head>' >> "index.html"
+echo '<body>' >> "index.html"
+echo '</body>' >> "index.html"
+echo '</html>' >> "index.html"
